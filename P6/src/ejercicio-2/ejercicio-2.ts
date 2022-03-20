88d894e6917b04924bebf6901098226fa5327e7a
Samuel Martín <alu0101359526@ull.edu.es>
2022-03-20 12:08:03 +0000

    Advanced

--- P6/src/ejercicio-2/ejercicio-2.ts
+++ P6/src/ejercicio-2/ejercicio-2.ts
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable new-cap */
+/* eslint-disable prefer-const */
 interface streamable<T> {
   name: string;
   age: number;
@@ -49,17 +52,63 @@ class series extends basicStreamableCollection implements
       auxiliaryResult.push(this.seriesArray[i].name);
     }
     result = auxiliaryResult.sort();
+    let finalResult: any[] = [];
+    let j: number = 0;
+    for (i = 0; i < this.seriesArray.length; i++) {
+      for (j = 0; j < this.seriesArray.length; j++) {
+        if (result[i] === this.seriesArray[j].name) {
+          finalResult.push(this.seriesArray[j]);
+        }
+      }
+    }
     console.log();
     // eslint-disable-next-line max-len
     console.log(`La lista de series del catálogo ordenada por orden alfabético es: `);
-    console.log(result);
+    console.log(finalResult);
   }
   ageFilter(): any {
     let i: number = 0;
     let auxiliaryResult: any[] = [];
+    let result: any[] = [];
+    for (i = 0; i < this.seriesArray.length; i++) {
+      auxiliaryResult.push(this.seriesArray[i].age);
+    }
+    result = auxiliaryResult.sort();
+    let finalResult: any[] = [];
+    let j: number = 0;
+    for (i = 0; i < this.seriesArray.length; i++) {
+      for (j = 0; j < this.seriesArray.length; j++) {
+        if (result[i] === this.seriesArray[j].age) {
+          finalResult.push(this.seriesArray[j]);
+        }
+      }
+    }
+    console.log();
+    // eslint-disable-next-line max-len
+    console.log(`La lista de serie del catálogo ordenada por orden de año de estreno es: `);
+    console.log(finalResult);
   }
   categoryFilter(): any {
+    let i: number = 0;
+    let auxiliaryResult: any[] = [];
+    let result: any[] = [];
+    for (i = 0; i < this.seriesArray.length; i++) {
+      auxiliaryResult.push(this.seriesArray[i].category);
+    }
+    result = auxiliaryResult.sort();
+    let finalResult: any[] = [];
+    let j: number = 0;
+    for (i = 0; i < this.seriesArray.length; i++) {
+      for (j = 0; j < this.seriesArray.length; j++) {
+        if (result[i] === this.seriesArray[j].category) {
+          finalResult.push(this.seriesArray[j]);
+        }
+      }
+    }
     console.log();
+    // eslint-disable-next-line max-len
+    console.log(`La lista de serie del catálogo ordenada por orden categoría es: `);
+    console.log(finalResult);
   }
 }
 
@@ -72,13 +121,70 @@ class films extends basicStreamableCollection implements streamable_film<any> {
     this.FilmsArray.push(filmAdd);
   }
   nameFilter(): any {
+    let i: number = 0;
+    let auxiliaryResult: any[] = [];
+    let result: any[] = [];
+    for (i = 0; i < this.FilmsArray.length; i++) {
+      auxiliaryResult.push(this.FilmsArray[i].name);
+    }
+    result = auxiliaryResult.sort();
+    let finalResult: any[] = [];
+    let j: number = 0;
+    for (i = 0; i < this.FilmsArray.length; i++) {
+      for (j = 0; j < this.FilmsArray.length; j++) {
+        if (result[i] === this.FilmsArray[j].name) {
+          finalResult.push(this.FilmsArray[j]);
+        }
+      }
+    }
     console.log();
+    // eslint-disable-next-line max-len
+    console.log(`La lista de películas del catálogo ordenada por orden alfabético es: `);
+    console.log(finalResult);
   }
   ageFilter(): any {
+    let i: number = 0;
+    let auxiliaryResult: any[] = [];
+    let result: any[] = [];
+    for (i = 0; i < this.FilmsArray.length; i++) {
+      auxiliaryResult.push(this.FilmsArray[i].age);
+    }
+    result = auxiliaryResult.sort();
+    let finalResult: any[] = [];
+    let j: number = 0;
+    for (i = 0; i < this.FilmsArray.length; i++) {
+      for (j = 0; j < this.FilmsArray.length; j++) {
+        if (result[i] === this.FilmsArray[j].age) {
+          finalResult.push(this.FilmsArray[j]);
+        }
+      }
+    }
     console.log();
+    // eslint-disable-next-line max-len
+    console.log(`La lista de películas del catálogo ordenada por orden de año de estreno es: `);
+    console.log(finalResult);
   }
   categoryFilter(): any {
+    let i: number = 0;
+    let auxiliaryResult: any[] = [];
+    let result: any[] = [];
+    for (i = 0; i < this.FilmsArray.length; i++) {
+      auxiliaryResult.push(this.FilmsArray[i].category);
+    }
+    result = auxiliaryResult.sort();
+    let finalResult: any[] = [];
+    let j: number = 0;
+    for (i = 0; i < this.FilmsArray.length; i++) {
+      for (j = 0; j < this.FilmsArray.length; j++) {
+        if (result[i] === this.FilmsArray[j].category) {
+          finalResult.push(this.FilmsArray[j]);
+        }
+      }
+    }
     console.log();
+    // eslint-disable-next-line max-len
+    console.log(`La lista de películas del catálogo ordenada por orden categoría es: `);
+    console.log(finalResult);
   }
 }
 
@@ -93,13 +199,70 @@ class documentary extends basicStreamableCollection
     this.documentaryArray.push(documentaryAdd);
   }
   nameFilter(): any {
+    let i: number = 0;
+    let auxiliaryResult: any[] = [];
+    let result: any[] = [];
+    for (i = 0; i < this.documentaryArray.length; i++) {
+      auxiliaryResult.push(this.documentaryArray[i].name);
+    }
+    result = auxiliaryResult.sort();
+    let finalResult: any[] = [];
+    let j: number = 0;
+    for (i = 0; i < this.documentaryArray.length; i++) {
+      for (j = 0; j < this.documentaryArray.length; j++) {
+        if (result[i] === this.documentaryArray[j].name) {
+          finalResult.push(this.documentaryArray[j]);
+        }
+      }
+    }
     console.log();
+    // eslint-disable-next-line max-len
+    console.log(`La lista de documentales del catálogo ordenada por orden alfabético es: `);
+    console.log(finalResult);
   }
   ageFilter(): any {
+    let i: number = 0;
+    let auxiliaryResult: any[] = [];
+    let result: any[] = [];
+    for (i = 0; i < this.documentaryArray.length; i++) {
+      auxiliaryResult.push(this.documentaryArray[i].age);
+    }
+    result = auxiliaryResult.sort();
+    let finalResult: any[] = [];
+    let j: number = 0;
+    for (i = 0; i < this.documentaryArray.length; i++) {
+      for (j = 0; j < this.documentaryArray.length; j++) {
+        if (result[i] === this.documentaryArray[j].age) {
+          finalResult.push(this.documentaryArray[j]);
+        }
+      }
+    }
     console.log();
+    // eslint-disable-next-line max-len
+    console.log(`La lista de documentales del catálogo ordenada por orden de año de estreno es: `);
+    console.log(finalResult);
   }
   categoryFilter(): any {
+    let i: number = 0;
+    let auxiliaryResult: any[] = [];
+    let result: any[] = [];
+    for (i = 0; i < this.documentaryArray.length; i++) {
+      auxiliaryResult.push(this.documentaryArray[i].category);
+    }
+    result = auxiliaryResult.sort();
+    let finalResult: any[] = [];
+    let j: number = 0;
+    for (i = 0; i < this.documentaryArray.length; i++) {
+      for (j = 0; j < this.documentaryArray.length; j++) {
+        if (result[i] === this.documentaryArray[j].category) {
+          finalResult.push(this.documentaryArray[j]);
+        }
+      }
+    }
     console.log();
+    // eslint-disable-next-line max-len
+    console.log(`La lista de documentales del catálogo ordenada por orden categoría es: `);
+    console.log(finalResult);
   }
 }
 
@@ -113,8 +276,27 @@ const gameOfThrones =
 const totallySeries =
   new series(``, 0, ``, ``, 0, []);
 
-totallySeries.addSeries(sexEducation);
-totallySeries.addSeries(gameOfThrones);
-totallySeries.nameFilter();
+// totallySeries.addSeries(sexEducation);
+// totallySeries.addSeries(gameOfThrones);
+// totallySeries.nameFilter();
+// totallySeries.ageFilter();
+// totallySeries.categoryFilter();
+
+const starWars =
+  new films(`Star Wars`, 1977, `Película`, `Ciencia Ficción`,
+      `George Lucas`, []);
+
+const backToTheFuture =
+  new films(`Regreso al Futuro`, 1985, `Película`, `Ciencia Ficción`,
+      `Bob Gale`, []);
+
+const totallyFilms =
+  new films(``, 0, ``, ``, ``, []);
+
+totallyFilms.addFilm(starWars);
+totallyFilms.addFilm(backToTheFuture);
+totallyFilms.nameFilter();
+totallyFilms.ageFilter();
+totallyFilms.categoryFilter();
 
 
