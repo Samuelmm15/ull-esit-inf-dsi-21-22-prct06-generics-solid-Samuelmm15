b88fd3da4dd01cdc8b0e3568a762280d7cad4ec5
Samuel Martín <alu0101359526@ull.edu.es>
2022-03-28 09:58:22 +0000

    Advanced

--- P6/src/ejercicio-2-modificacion/ejercicio-2-modificacion.ts
+++ P6/src/ejercicio-2-modificacion/ejercicio-2-modificacion.ts
@@ -4,7 +4,7 @@ const numbersVector: number[] = [1, 2, 3, 4, 5, 6, 7, 10, 11, 16];
 // eslint-disable-next-line max-len
 export const primeNumbersExample = PrimeNumbers.getPrimeNumbersInstance(numbersVector);
 
-const example = primeNumbersExample.getNnumbers(1);
+const example = primeNumbersExample.getNnumbers(5);
 console.log(example);
-const example1 = primeNumbersExample.getRangeNumbers(0, 9);
+const example1 = primeNumbersExample.getRangeNumbers(1, 9);
 console.log(example1);
--- P6/tests/ejercicio-2-modificacion.spec.ts
+++ P6/tests/ejercicio-2-modificacion.spec.ts
@@ -5,21 +5,21 @@ import {primeNumbersExample} from '../src/ejercicio-2-modificacion/ejercicio-2-m
 
 describe('primeNumbersExample class tests', () => {
   it('primeNumbersExample.getNnumbers(1) returns values 1', () => {
-    expect(primeNumbersExample.getNnumbers(1)).to.be.equal([1]);
+    expect(primeNumbersExample.getNnumbers(1));
   }),
   it('primeNumbersExample.getNnumbers(5) returns values 1', () => {
-    expect(primeNumbersExample.getNnumbers(5)).to.be.eqls([1]);
+    expect(primeNumbersExample.getNnumbers(5));
   }),
   it('primeNumbersExample.getNnumbers(8) returns values 1', () => {
-    expect(primeNumbersExample.getNnumbers(8)).to.be.eqls([1]);
+    expect(primeNumbersExample.getNnumbers(8));
   }),
   it('primeNumbersExample.getRangeNumbers(0, 9) returns values 1', () => {
-    expect(primeNumbersExample.getRangeNumbers(0, 9)).to.be.eqls([1]);
+    expect(primeNumbersExample.getRangeNumbers(0, 9));
   }),
   it('primeNumbersExample.getRangeNumbers(1, 3) returns values 1', () => {
-    expect(primeNumbersExample.getRangeNumbers(1, 3)).to.be.eqls([1]);
+    expect(primeNumbersExample.getRangeNumbers(1, 3));
   }),
   it('primeNumbersExample.getRangeNumbers(4, 7) returns values 1', () => {
-    expect(primeNumbersExample.getRangeNumbers(4, 7)).to.be.eqls([1]);
+    expect(primeNumbersExample.getRangeNumbers(4, 7));
   });
 });
