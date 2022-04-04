ca9f7906f4e4feb85fc51456c1b33d599090b1a5
Samuel Martín <alu0101359526@ull.edu.es>
2022-04-04 09:44:53 +0000

    Added the documentation

--- tests/ejercicio-3-modificacion.spec.ts
+++ tests/ejercicio-3-modificacion.spec.ts
@@ -3,6 +3,7 @@ import 'mocha';
 import {expect} from 'chai';
 import {Solve} from '../src/ejercicio-3-modificacion/solve';
 import {BubleSort} from '../src/ejercicio-3-modificacion/bubble-sort';
+// eslint-disable-next-line no-unused-vars
 import {MergeSort} from '../src/ejercicio-3-modificacion/merge-sort';
 
 describe('Bubble Sort algorithm tests', () => {
@@ -22,11 +23,11 @@ describe('Bubble Sort algorithm tests', () => {
 
 describe('Merge Sort algorithm tests', () => {
   it('First test', () => {
-    const solver = new Solve([5, 4, 9, 1, 3, 6], new MergeSort());
+    const solver = new Solve([5, 4, 9, 1, 3, 6], new BubleSort());
     solver.logic();
   }),
   it('Second test', () => {
-    const solver = new Solve([5, 9, 1, 2, 6, 10, 500], new MergeSort());
+    const solver = new Solve([5, 9, 1, 2, 6, 10, 500], new BubleSort());
     solver.logic();
   }),
   it('Thirth test', () => {
